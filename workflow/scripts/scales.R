2828709e0e0b6266d89e078bf68351d4be3ec29c
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-07-31 10:59:41 +0100

    Create assets branch

--- workflow/scripts/scales.R
+++
@@ -1,8 +0,0 @@
-library(scran)
-
-D<-as.matrix(read.csv(snakemake@input[[1]],header=TRUE,row.names=1))
-
-clusters <- quickCluster(D)
-sce <- calculateSumFactors(D, clusters=clusters)
-
-write.table(sce,snakemake@output[[1]],sep=',',row.names=FALSE,col.names=FALSE)
